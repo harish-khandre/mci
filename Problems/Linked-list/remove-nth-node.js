2b81a14e476716bfaf8009952cea250108320924
Harish-Khandre <harishkhandre2901@gmail.com>
2024-01-02 02:57:48 +0530

    remove nth node

--- Problems/Linked-list/remove-nth-node.js
+++ Problems/Linked-list/remove-nth-node.js
@@ -1,17 +1,27 @@
-const removeNthFromEnd = (head, n) => {
-  const traverseToIndex = (n) => {
-    let counter = 0;
-    let currentNode = head;
-    while (counter !== n) {
-      currentNode = currentNode.next;
-      counter++;
-    }
-    currentNode;
-  };
+/* Given the head of a linked list, remove the nth node from the end of the list and return its head.
 
-  let temp = currentNode.next;
-  currentNode.next = currentNode.prev;
-  currentNode.prev = currentNode;
-  currentNode = null;
-  return head;
+Example:
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5] */
+
+const removeNthFromEnd = (head, n) => {
+  let dummy = new ListNode(0);
+  dummy.next = head;
+
+  let left = dummy;
+  let right = head;
+
+  while (right && n > 0) {
+    right = right.next;
+    n--;
+  }
+
+  while (right) {
+    // runs this loop till the right is reached the end of the list
+    left = left.next;
+    right = right.next;
+  }
+  // jumping two numbers to get nth point
+  left.next = left.next.next;
+  return dummy.next;
 };
