163c716beb450b8ed7b27a7e869596ae4f4f2388
Harish-Khandre <harishkhandre2901@gmail.com>
2024-01-09 17:33:55 +0530

    BTBFS

---
+++ Practise-stuff/BTBFS.ts
@@ -0,0 +1,17 @@
+// Breadth first search, searches by level like a horizontal search line by line
+// BFS does not preserve the shape
+export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+  const q: (BinaryNode<number> | null)[] = [head];
+  while (q.length) {
+    const curr = q.shift() as BinaryNode<number> | undefined | null;
+    if (!curr) {
+      continue;
+    }
+    if (curr.value === needle) {
+      return true;
+    }
+    q.push(curr.left);
+    q.push(curr.right);
+  }
+  return false;
+}
