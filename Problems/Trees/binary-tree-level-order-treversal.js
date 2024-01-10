5d325ef619d020f9978151624801a1e1b50815a2
Harish-Khandre <harishkhandre2901@gmail.com>
2024-01-10 16:23:02 +0530

    btlot

---
+++ Problems/Trees/binary-tree-level-order-treversal.js
@@ -0,0 +1,21 @@
+const levelOrder = (root) => {
+  if (root === null) return [];
+
+  const res = [];
+  const queue = [root];
+
+  while (queue.length) {
+    const levelSize = queue.length;
+    const levelArr = [];
+
+    for (let i = 0; i < levelSize; i++) {
+      const current = queue.shift();
+      levelArr.push(current.val);
+
+      if (current.left) queue.push(current.left);
+      if (current.right) queue.push(current.right);
+    }
+    res.push(levelArr);
+  }
+  return res;
+};
