2f0105b0837653efc8e22aafdbb855cd5a485aed
Harish-Khandre <harishkhandre2901@gmail.com>
2024-01-18 10:30:07 +0530

    word search

--- Problems/Backtracking/word-search.js
+++ Problems/Backtracking/word-search.js
@@ -4,7 +4,7 @@ const exist = (board, word) => {
   const n = board[0].length;
 
   // Helper function to perform DFS search for the word
-  const check = (row, col, index) => {
+  const dfs = (row, col, index) => {
     // If the entire word is found, return true
     if (index === word.length) return true;
 
@@ -22,12 +22,12 @@ const exist = (board, word) => {
     const originalChar = board[row][col];
     board[row][col] = "*";
 
-    // Recursively check neighboring cells
+    // Recursively dfs neighboring cells
     const result =
-      check(row + 1, col, index + 1) ||
-      check(row - 1, col, index + 1) ||
-      check(row, col + 1, index + 1) ||
-      check(row, col - 1, index + 1);
+      dfs(row + 1, col, index + 1) ||
+      dfs(row - 1, col, index + 1) ||
+      dfs(row, col + 1, index + 1) ||
+      dfs(row, col - 1, index + 1);
 
     // Revert the current cell back to its original state
     board[row][col] = originalChar;
@@ -39,7 +39,7 @@ const exist = (board, word) => {
   for (let row = 0; row < m; row++) {
     for (let col = 0; col < n; col++) {
       // If the word is found starting from the current cell, return true
-      if (check(row, col, 0)) return true;
+      if (dfs(row, col, 0)) return true;
     }
   }
 
