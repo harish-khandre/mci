d6c64383e38439468cf8d90ef30da1b1b38be666
harish-khandre <harishkhandre2901@gmail.com>
2024-02-10 18:17:04 +0530

    new version

--- Problems/Dynamic-programming/decode-ways.js
+++ Problems/Dynamic-programming/decode-ways.js
@@ -1,29 +1,11 @@
-var numDecodings = (str, index = 0, memo = new Map()) => {
-  const isBaseCase1 = !str.length || str[index] === "0";
-  if (isBaseCase1) return 0;
+const numDecodings = (s) => {
+  const dp = new Array(s.length + 1).fill(0);
+  dp[s.length] = 1;
 
-  const isisBaseCase2 = index === str.length;
-  if (isisBaseCase2) return 1;
-
-  if (memo.has(index)) return memo.get(index);
-
-  return dfs(str, index, memo);
-};
-
-const dfs = (str, index, memo) => {
-  let count = numDecodings(str, index + 1, memo);
-
-  if (isTwoDigit(str, index)) {
-    count += numDecodings(str, index + 2, memo);
+  for (let i = s.length - 1; i >= 0; i--) {
+    if (s[i] == 0) continue;
+    dp[i] = dp[i + 1];
+    if (s[i] + s[i + 1] <= 26) dp[i] += dp[i + 2];
   }
-
-  memo.set(index, count);
-
-  return count;
-};
-
-var isTwoDigit = (str, index) => {
-  const twoDigit = Number(str.slice(index, index + 2));
-
-  return 10 <= twoDigit && twoDigit <= 26;
+  return dp[0];
 };
