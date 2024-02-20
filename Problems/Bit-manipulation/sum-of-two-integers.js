251acc4fa7fe501d08fcf86a14a55b0886f7f8cb
harish-khandre <harishkhandre2901@gmail.com>
2024-02-20 22:28:20 +0530

    comments

--- Problems/Bit-manipulation/sum-of-two-integers.js
+++ Problems/Bit-manipulation/sum-of-two-integers.js
@@ -1,8 +1,17 @@
+// Define a function called getSum which takes two parameters, a and b
 const getSum = (a, b) => {
+  // Execute a while loop until b is not equal to 0
   while (b !== 0) {
+    // Calculate the bitwise XOR of a and b and store it in xor
+    // Calculate the carry by performing a bitwise AND operation on a and b, then shifting the result one position to the left
     const [xor, carry] = [a ^ b, (a & b) << 1];
+
+    // Update the value of a to be the calculated XOR value
     a = xor;
+
+    // Update the value of b to be the calculated carry value
     b = carry;
   }
+  // Return the final value of a
   return a;
 };
