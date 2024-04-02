23fd2707a03f8d7d37293a63e2c5e6f30e5e710d
haris-khandre <harishkhandre2901@gmail.com>
2024-04-02 08:33:38 +0530

    btbnss2

---
+++ Problems/Sliding-window/best-time-to-buy-and-sell-stock-ii.js
@@ -0,0 +1,9 @@
+const MaxProfit = (prices) => {
+  let maxProfit = 0;
+  for (let i = 1; i < prices.length; ++i) {
+    if (prices[i] > prices[i - 1]) {
+      maxProfit += prices[i] - prices[i - 1];
+    }
+  }
+  return maxProfit;
+};
